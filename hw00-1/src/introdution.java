e4f8df63d7713eaa2ac87a6a5052bd3fa0a108ad
caseLearner0718 <105021025@live.asia.edu.tw>
2016-09-12 15:50:20 +0800

    homework submit

---
+++ hw00-1/src/introdution.java
@@ -0,0 +1,13 @@
+
+public class introdution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hihi");
+System.out.println("I'm linus");
+	}
+
+}
